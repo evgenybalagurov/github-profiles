de5538b60d437eb35e9ab6d0d6ca12e3a20fb8ae
evgeniy balagurov <evgeniy.balagurov@yandex.ru>
2023-01-17 13:21:09 +0400

    add statistics component

---
+++ src/mock/index.ts
@@ -0,0 +1,17 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  "login": "evgenybalagurov",
+  "id": 89060313,
+  "avatar": "https://avatars.githubusercontent.com/u/89060313?v=4",
+  "name": "Evgeny Balagurov",
+  "company": "null",
+  "blog": "",
+  "location": "null",
+  "bio": "null",
+  "twitter": "null",
+  "repos": 4,
+  "followers": 4,
+  "following": 7,
+  "created": "2021-08-17T04:45:44Z",
+}
